45a5fdcdcab4ee83c8685a0d1901333405870466
Kris Chen <cw.akira@gmail.com>
2015-06-02 18:46:21 +0200

    cw

--- PaintCodeTutorial/ViewController.swift
+++ PaintCodeTutorial/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var stopWatchHandView: StopwatchHand!
     override func viewDidLoad() {
         super.viewDidLoad()
+        println("Stop Watch")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
